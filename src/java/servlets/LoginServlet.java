a308fbb2e51ec00da375c3c1d1f3990065cf6ec2
Dohui Lee <dohui.lee@edu.sait.ca>
2020-10-12 09:44:48 -0600

    Made project open

--- src/java/servlets/LoginServlet.java
+++ src/java/servlets/LoginServlet.java
@@ -12,7 +12,7 @@ public class LoginServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
+        getServletContext().getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
     }
 
     @Override
